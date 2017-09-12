49f9fe71152b1e62b9f5b200a0c423cfd561aa7a
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-09-12 14:18:12 +0300

    C Apps: Perform both a coap and coaps query

--- c-apps/client.c
+++ c-apps/client.c
@@ -5,30 +5,43 @@
 
 #define DESIRED_RESOURCE "/a/high-level-example"
 
-static char *desiredUrl = NULL;
-
 static OCStackApplicationResult
-requestCallback(void *NOTHING, OCDoHandle handle, OCClientResponse *response) {
-	printf("response->result: %d\n", response ? response->result : -1);
+requestCallback(void *url, OCDoHandle handle, OCClientResponse *response) {
+	printf("%s: response->result: %d\n", (char *)url,
+			response ? response->result : -1);
 	return OC_STACK_KEEP_TRANSACTION;
 }
 
 static void
-performRequest() {
+performRequest(const char *desiredUrl) {
 	OCDoHandle handle = NULL;
 	OCCallbackData callback = {
-		.context = NULL,
+		.context = desiredUrl,
 		.cb = requestCallback,
-		.cd = NULL,
+		.cd = free,
 	};
 	printf("OCDoResource(request) to %s: %d\n", desiredUrl,
 		OCDoResource(&handle, OC_REST_GET, desiredUrl, NULL,
 			NULL, CT_DEFAULT, OC_HIGH_QOS, &callback, NULL, 0));
-	free(desiredUrl);
+}
+
+static char *epToUrl(OCEndpointPayload *ep) {
+	char *percent = strstr(ep->addr, "%");
+	if (percent) {
+		*percent = 0;
+	}
+	return strdup_printf("%s://%s%s%s:%d%s",
+		ep->tps,
+		(ep->family & OC_IP_USE_V6 ? "[" : ""),
+		ep->addr,
+		(ep->family & OC_IP_USE_V6 ? "]" : ""),
+		ep->port,
+		DESIRED_RESOURCE);
 }
 
 static OCStackApplicationResult
 discoverCallback(void *NOTHING, OCDoHandle handle, OCClientResponse *response) {
+	char *coapsUrl = NULL, *coapUrl = NULL;
 	OCResourcePayload *resource = NULL;
 	if (response && response->result == OC_STACK_OK && response->payload &&
 			response->payload->type == PAYLOAD_TYPE_DISCOVERY) {
@@ -36,28 +49,22 @@ discoverCallback(void *NOTHING, OCDoHandle handle, OCClientResponse *response) {
 				resource; resource = resource->next) {
 			if (resource->uri && !strcmp(resource->uri, DESIRED_RESOURCE)) {
 				for (OCEndpointPayload *ep = resource->eps; ep; ep = ep->next) {
-					if (!desiredUrl && !strcmp(ep->tps, "coaps")) {
-						char *percent = strstr(ep->addr, "%");
-						if (percent) {
-							*percent = 0;
-						}
-						desiredUrl = strdup_printf("%s://%s%s%s:%d%s",
-							ep->tps,
-							(ep->family & OC_IP_USE_V6 ? "[" : ""),
-							ep->addr,
-							(ep->family & OC_IP_USE_V6 ? "]" : ""),
-							ep->port,
-							DESIRED_RESOURCE);
+					if (!coapsUrl && !strcmp(ep->tps, "coaps")) {
+						coapsUrl = epToUrl(ep);
+					} else if (!coapUrl && !strcmp(ep->tps, "coap")) {
+						coapUrl = epToUrl(ep);
 					}
-					printf("Resource endpoint: %s://%s%s%s:%d\n",
-						ep->tps,
-						(ep->family & OC_IP_USE_V6 ? "[" : ""),
-						ep->addr,
-						(ep->family & OC_IP_USE_V6 ? "]" : ""),
-						ep->port);
+					if (coapsUrl && coapUrl) {
+						break;
+					}
+				}
+				if (coapsUrl) {
+					performRequest(coapsUrl);
+				}
+				if (coapUrl) {
+					performRequest(coapUrl);
 				}
 				printf("Resource found\n");
-				performRequest();
 				return OC_STACK_DELETE_TRANSACTION;
 			}
 		}
